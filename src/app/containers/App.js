dc49a69de59e8d2fd0e36ab89753010e940e405e
avinash-dev <avinash@teletext-holidays.co.uk>
2019-10-17 16:23:42 +0530

    added prism js for <code> highlighting

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -11,6 +11,65 @@ const App = props => (
         <Main changeUsername={() => props.setName("Anna")} />
             <br/>
         <User username={props.user.name} />
+        <div className="row">
+            <div className="col-xs-3">
+                <pre>
+                    <code className="language-js">
+                    {`
+                        // UserReducer
+                        const userReducer = (state = {
+                            name: "Max", age: 27
+                        }, action) => {
+                            switch (action.type) {
+                                case "SET_NAME_FULFILLED": 
+                                    state = {
+                                        ...state,
+                                        name: action.payload,
+                                    };
+                                    break;
+                                case "SET_AGE":
+                                        state = {
+                                            ...state,
+                                            age: action.payload
+                                        };
+                                        break;    
+                                default:
+                            }
+                            return state;
+                        }
+                        export default userReducer;
+                    `}
+                    </code>
+                </pre>
+            </div>
+            <div className="col-xs-6 offset-xs-1">
+                <pre>
+                    <code className="language-js">
+                        {`
+                        // userActions.js
+                        export function setName(name){
+                            // return dispatch => {
+                            //     setTimeout(() => {
+                            //         dispatch({
+                            //             type: 'SET_NAME',
+                            //             payload: name
+                            //         })
+                            //     }, 2000);
+                            // }
+                            return {
+                                type: "SET_NAME",
+                                payload: new Promise((resolve, reject) => {
+                                    setTimeout(() => {
+                                        resolve(name)
+                                    }, 2000);
+                                })
+                            }
+                        }
+                        `}
+                    </code>
+                </pre>
+            </div>
+        </div>
     </div>
 )
 
